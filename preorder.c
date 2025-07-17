bd3e1bc4721fdfcb56fee9690bf0eff864f28ea7
kowshik kumar sarker <sarkerkowshik016@gmail.com>
2025-07-18 03:13:00 +0600

    Add files via upload

---
+++ preorder.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *makenode(int data)
+{
+    struct node *n; 
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void preOrder(struct node*root){
+    if(root!=NULL){
+        printf("%d",root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+int main()
+{
+
+
+    struct node *p = makenode(4);
+    struct node *p1 = makenode(1);
+    struct node *p2 = makenode(6);
+    struct node *p3 = makenode(5);
+    struct node *p4 = makenode(2);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+preOrder(p);
+    
+
+    return 0;
+}
